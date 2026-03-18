f897a8978820b70a500c1b3260564d432d378141
Shijie Li <30463638+Shijie-NLP@users.noreply.github.com>
2026-03-18 19:46:54 +1100

    add custom postprocessor

--- src/zoo/dome/postprocessor.py
+++ src/zoo/dome/postprocessor.py
@@ -24,7 +24,12 @@ class DomePostProcessor(nn.Module):
     __share__ = ["num_classes", "use_focal_loss", "num_top_queries", "remap_mscoco_category"]
 
     def __init__(
-        self, num_classes=80, use_focal_loss=True, num_top_queries=1500, remap_mscoco_category=False, score_thresh=0.1
+        self,
+        num_classes=80,
+        use_focal_loss=True,
+        num_top_queries=1500,
+        remap_mscoco_category=False,
+        score_thresh=0.1,
     ) -> None:
         super().__init__()
         self.use_focal_loss = use_focal_loss
@@ -87,9 +92,59 @@ class DomePostProcessor(nn.Module):
 
         return results
 
-    def deploy(
-        self,
-    ):
+    def deploy(self):
         self.eval()
         self.deploy_mode = True
         return self
+
+
+@register()
+class CustomPostProcessor(DomePostProcessor):
+    def forward(self, outputs, orig_target_sizes: torch.Tensor):
+        logits, boxes = outputs["pred_logits"], outputs["pred_boxes"]
+
+        # import pdb
+        # pdb.set_trace()
+
+        self.num_top_queries = logits.shape[1]  # cancel num_top_queries
+        # orig_target_sizes = torch.stack([t["orig_size"] for t in targets], dim=0)
+        bbox_pred = torchvision.ops.box_convert(boxes, in_fmt="cxcywh", out_fmt="xyxy")
+        bbox_pred *= orig_target_sizes.repeat(1, 2).unsqueeze(1)
+
+        cls_idx = 3
+
+        if self.use_focal_loss:
+            scores = F.sigmoid(logits[..., cls_idx])
+            # TODO for older tensorrt
+            # labels = index % self.num_classes
+            labels = torch.ones_like(scores, dtype=torch.int64) * cls_idx  # TODO, move this into coco eval
+            boxes = bbox_pred
+
+        else:
+            scores = F.softmax(logits)[:, :, :-1]
+            scores, labels = scores.max(dim=-1)
+            if scores.shape[1] > self.num_top_queries:
+                scores, index = torch.topk(scores, self.num_top_queries, dim=-1)
+                labels = torch.gather(labels, dim=1, index=index)
+                boxes = torch.gather(boxes, dim=1, index=index.unsqueeze(-1).tile(1, 1, boxes.shape[-1]))
+
+        # TODO for onnx export
+        if self.deploy_mode:
+            return labels, boxes, scores
+
+        # TODO
+        if self.remap_mscoco_category:
+            from ...data.dataset import mscoco_label2category
+
+            labels = (
+                torch.tensor([mscoco_label2category[int(x.item())] for x in labels.flatten()])
+                .to(boxes.device)
+                .reshape(labels.shape)
+            )
+
+        results = []
+        for lab, box, sco in zip(labels, boxes, scores):
+            result = dict(labels=lab, boxes=box, scores=sco)
+            results.append(result)
+
+        return results
