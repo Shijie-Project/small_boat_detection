cea8a74a7a661ce151e5a35071895542a932e11d
Shijie Li <30463638+Shijie-NLP@users.noreply.github.com>
2026-06-18 18:28:49 +1000

    add evaluation modules: implement AitodCocoEvaluatorSlow, VisdroneCocoEvaluator, VOCEvaluator, and coco_utils for enhanced dataset evaluation

---
+++ src/data/dataset/voc_eval.py
@@ -0,0 +1,9 @@
+"""
+Copied from D-FINE (https://github.com/Peterande/D-FINE)
+Copyright(c) 2024 The D-FINE Authors. All Rights Reserved.
+"""
+
+
+class VOCEvaluator:
+    def __init__(self) -> None:
+        pass
